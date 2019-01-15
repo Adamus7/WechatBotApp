b61f2c9e85ade6d42ba3563882afb6d63c805be2
Adamus7 <adamus_7@outlook.com>
2019-01-15 23:23:01 +0800

    Draft and test the Bot Direct Connector. Need further review and code refactoring

--- RayWechatApp/Controllers/WxEndpointController.cs
+++ RayWechatApp/Controllers/WxEndpointController.cs
@@ -10,6 +10,9 @@ using System.Xml;
 using WxUtil;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.DirectLine;
+
 
 namespace RayWechatApp.Controllers
 {
@@ -18,9 +21,18 @@ namespace RayWechatApp.Controllers
     public class WxEndpointController : ControllerBase
     {
         private string WxToken;
+        private string directLineSecret;
+        private string botId;
+        private string fromUser;
+        //private DirectLineClient botClient;
+        //private Conversation testCon;
         public WxEndpointController(IConfiguration Configuration)
         {
             WxToken = Configuration["Token"];
+            directLineSecret = Configuration["DirectLineSecret"];
+            botId = Configuration["BotId"];
+            fromUser = Configuration["fromUser"];
+            myConMgr.botClient = new DirectLineClient(directLineSecret);
         }
 
         [HttpGet("")]
@@ -36,30 +48,61 @@ namespace RayWechatApp.Controllers
             }
         }
         [HttpPost("")]
-        public string WxPost([FromQuery]string signature, [FromQuery]string nonce, [FromQuery]string timestamp)
+        public async Task<string> WxPost([FromQuery]string signature, [FromQuery]string nonce, [FromQuery]string timestamp)
         {
-            ////if (IsMessageFromWX(signature, nonce, timestamp, WxToken))
-            ////{
-            ////}
-            using (var reader = new StreamReader(Request.Body))
+            if (WXHelper.IsMessageFromWX(signature, nonce, timestamp, WxToken) || true)
             {
-                var body = reader.ReadToEnd();
-                if(String.IsNullOrEmpty(body))
+                using (var reader = new StreamReader(Request.Body))
                 {
-                    return "Failed to get message";
-                }
-                
-                WXMsg msg = WXHelper.ParseWXMsgFromBodyString(body);
-                string wxId = msg.FromUserName;
-                string responseXML = "";
+                    var body = reader.ReadToEnd();
+                    if (String.IsNullOrEmpty(body))
+                    {
+                        return "Failed to get message";
+                    }
 
-                if (msg.MsgType == WXMsgType.Text)
-                {
-                    responseXML = WXHelper.ConstructWXTextMessage(msg, $"你说是{msg.Content}");
+                    WXMsg msg = WXHelper.ParseWXMsgFromBodyString(body);
+                    string wxId = msg.FromUserName;
+                    string responseXML = "";
+
+                    if (msg.MsgType == WXMsgType.Text)
+                    {
+                        if (myConMgr.testCon == null)
+                        {
+                            myConMgr.testCon = await myConMgr.botClient.Conversations.StartConversationAsync();
+                        }
+                        Activity userMessage = new Activity
+                        {
+                            From = new ChannelAccount(fromUser),
+                            Text = msg.Content,
+                            Type = ActivityTypes.Message
+                        };
+                        await myConMgr.botClient.Conversations.PostActivityAsync(myConMgr.testCon.ConversationId, userMessage);
+
+                        var activitySet = await myConMgr.botClient.Conversations.GetActivitiesAsync(myConMgr.testCon.ConversationId, null);
+                        var activities = from x in activitySet.Activities
+                                         where x.From.Id == botId
+                                         select x;
+                        var returnString = "";
+                        foreach (Activity activity in activities)
+                        {
+                            returnString += activity.Text + "||";
+                        }
+                        responseXML = WXHelper.ConstructWXTextMessage(msg, returnString);
+                    }
+                    return responseXML;
                 }
-                return responseXML;
+            }
+            else
+            {
+                return "";
             }
         }
+
     }
 
+    public static class myConMgr
+    {
+        public static DirectLineClient botClient;
+        public static Conversation testCon;
+    }
 }
